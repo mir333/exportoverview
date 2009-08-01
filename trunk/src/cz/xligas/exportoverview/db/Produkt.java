8433998b8814f882b2b5bf0d628c90b5fbcb379f
miroslav.ligas <miroslav.ligas@9f4f964a-7ecc-11de-8c42-8d293cda74e2>
2009-08-01 23:36:37 +0000

    inicializacia

---
+++ trunk/src/cz/xligas/exportoverview/db/Produkt.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.xligas.exportoverview.db;
+
+/**
+ *
+ * @author xligas
+ */
+public class Produkt {
+
+}
