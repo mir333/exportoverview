8c1e73fe163a62668a4bfc2320d02ac717efac82
miroslav.ligas <miroslav.ligas@9f4f964a-7ecc-11de-8c42-8d293cda74e2>
2009-08-03 17:00:25 +0000

    clients apli

--- trunk/src/cz/ligas/exportoverview/appli/ClientOps.java
+++ trunk/src/cz/ligas/exportoverview/appli/ClientOps.java
@@ -1,9 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package cz.ligas.exportoverview.appli;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import cz.ligas.exportoverview.db.Clients;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Query;
 
 /**
  *
@@ -11,4 +14,43 @@ package cz.ligas.exportoverview.appli;
  */
 public class ClientOps {
 
+    private static EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("ExportOverviewPU");
+
+    public static void addClient(Clients client) throws Exception {
+        try {
+            EntityManager em = emFactory.createEntityManager();
+
+
+            em.getTransaction().begin();
+            em.persist(client);
+            em.getTransaction().commit();
+            em.close();
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
+    }
+
+    public static Clients getClientById(int id) throws Exception {
+        try {
+            EntityManager em = emFactory.createEntityManager();
+            Clients c = em.find(Clients.class, id);
+            em.close();
+            return c;
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
+    }
+
+    public static List<Clients> getClients() throws Exception {
+        try {
+            EntityManager em = emFactory.createEntityManager();
+            List<Clients> list = new ArrayList<Clients>();
+            Query q = em.createQuery("select c from Clients c order by c.id asc");
+            list = q.getResultList();
+            em.close();
+            return list;
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
+    }
 }
