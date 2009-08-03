50f9d123b49883bdd17d8539fabbf4000e3fb59f
miroslav.ligas <miroslav.ligas@9f4f964a-7ecc-11de-8c42-8d293cda74e2>
2009-08-03 13:27:32 +0000

    DB ready

---
+++ trunk/src/cz/ligas/exportoverview/db/Main.java
@@ -0,0 +1,106 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.ligas.exportoverview.db;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author xligas
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emFactory =
+                Persistence.createEntityManagerFactory("ExportOverviewPU");
+        EntityManager em = emFactory.createEntityManager();
+
+        ProductCategory pc = new ProductCategory();
+        pc.setCategoryCode("A");
+        pc.setCategoryName("test A category");
+
+
+        Products pro = new Products();
+        pro.setProductCode("A1");
+        pro.setProductName("testname");
+        pro.setProductDes("das");
+        pro.setProductPrice(123);
+        pro.setProductCategoryId(pc);
+
+
+        Products prod = new Products();
+        prod.setProductCode("A2");
+        prod.setProductName("testmeno");
+        prod.setProductDes("dasdas");
+        prod.setProductPrice(123);
+        prod.setProductCategoryId(pc);
+
+
+        Klients kl1 = new Klients();
+        kl1.setKlientName("fero");
+        kl1.setKlientAddress("fdsfs");
+
+        Klients kl2 = new Klients();
+        kl2.setKlientName("jozo");
+        kl2.setKlientAddress("fdsfs");
+
+
+        Export e = new Export();
+        e.setKlient(kl1);
+
+
+        Export e2 = new Export();
+        e2.setKlient(kl2);
+
+        ExportLine el = new ExportLine();
+        el.setProd(pro);
+        el.setSent(6);
+        el.setSold(3);
+        el.setExport(e);
+  
+        
+        
+
+        ExportLine el2 = new ExportLine();
+        el2.setProd(pro);
+        el2.setSent(1);
+        el2.setSold(2);
+        el2.setExport(e);
+
+        ExportLine el3 = new ExportLine();
+        el3.setProd(prod);
+        el3.setSent(3);
+        el3.setSold(4);
+        el3.setExport(e2);
+
+
+
+        System.err.println(pc.toString());
+        System.err.println("test");
+        System.err.println(pro.toString());
+
+
+        em.getTransaction().begin();
+        em.persist(pc);
+        em.persist(pro);
+        em.persist(prod);
+        em.persist(kl1);
+        em.persist(kl2);
+        em.persist(e);
+        em.persist(e2);
+        em.persist(el);
+        em.persist(el2);
+         em.persist(el3);
+        em.getTransaction().commit();
+
+        em.close();
+
+
+
+
+    }
+}
