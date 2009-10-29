09109b004a61ec4d37eaa0269f8f1705cb5ec927
miroslav.ligas <miroslav.ligas@9f4f964a-7ecc-11de-8c42-8d293cda74e2>
2009-10-29 17:27:08 +0000

    in progres

--- trunk/trunk/src/cz/ligas/exportoverview/db/Invoice.java
+++ trunk/trunk/src/cz/ligas/exportoverview/db/Invoice.java
@@ -6,10 +6,13 @@
 package cz.ligas.exportoverview.db;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 /**
  *
@@ -19,32 +22,35 @@ import javax.persistence.Id;
 public class Invoice implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+    @ManyToOne
+    private Clients client;
+    @OneToMany(mappedBy = "invoice")
+    private List<ExportLine> exportedProd;
 
-    public Long getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(int id) {
         this.id = id;
     }
 
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
+        hash += (int) id;
         return hash;
     }
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof Invoice)) {
             return false;
         }
         Invoice other = (Invoice) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+        if (this.id != other.id) {
             return false;
         }
         return true;
---
+++ trunk/trunk/src/cz/ligas/exportoverview/gui/InvoiceForm.java
@@ -0,0 +1,11 @@
+package cz.ligas.exportoverview.gui;
+
+/**
+ *
+ * @author xligas
+ */
+public class InvoiceForm extends DeliveryInvoiceForm {
+
+    public InvoiceForm() {
+    }
+}
