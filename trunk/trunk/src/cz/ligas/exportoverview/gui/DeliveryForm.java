85d9739fcfd977e8889ab467dd8c94e2d6c3aa00
miroslav.ligas <miroslav.ligas@9f4f964a-7ecc-11de-8c42-8d293cda74e2>
2009-11-02 18:03:44 +0000

    DeliveryInvoiceForm edited

--- trunk/trunk/src/cz/ligas/exportoverview/gui/DeliveryForm.java
+++ trunk/trunk/src/cz/ligas/exportoverview/gui/DeliveryForm.java
@@ -6,6 +6,7 @@ package cz.ligas.exportoverview.gui;
  */
 public class DeliveryForm extends DeliveryInvoiceForm {
     public DeliveryForm() {
-        
+        super("Delivery");
+        System.err.println("test1 delivery");
     }
 }
