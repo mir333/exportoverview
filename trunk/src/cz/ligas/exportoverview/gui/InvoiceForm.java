82c1598ba3b990a1e4d988b10697ba4f39158fae
miroslav.ligas <miroslav.ligas@9f4f964a-7ecc-11de-8c42-8d293cda74e2>
2009-11-25 13:55:16 +0000

    save

--- trunk/src/cz/ligas/exportoverview/gui/InvoiceForm.java
+++ trunk/src/cz/ligas/exportoverview/gui/InvoiceForm.java
@@ -1,52 +1,49 @@
 package cz.ligas.exportoverview.gui;
 
-import cz.ligas.exportoverview.appli.ExportLineOps;
-import cz.ligas.exportoverview.db.ExportLine;
 import cz.ligas.exportoverview.db.Invoice;
+import cz.ligas.exportoverview.appli.DeliveryOps;
+import cz.ligas.exportoverview.appli.InvoiceOps;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.beans.Beans;
 import java.util.Collections;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jdesktop.observablecollections.ObservableCollections;
 import org.jdesktop.observablecollections.ObservableList;
-
 /**
  *
  * @author xligas
  */
 public class InvoiceForm extends DocumentForm {
 
-    private List<ExportLine> exportLinesList;
-
     public InvoiceForm() {
-        super("Invoice","invoices");
+        super("Invoice", "invoices");
         myInit();
     }
 
     private void myInit() {
         try {
-            exportLinesList = Beans.isDesignTime() ? (ObservableList) Collections.emptyList() : ObservableCollections.observableList(ExportLineOps.getExportLine());
+            Invoice inv = (Invoice) docComboBox.getSelectedItem();
+            documentLinesList = Beans.isDesignTime() ? (ObservableList) Collections.emptyList() : ObservableCollections.observableList(InvoiceOps.getInvoiceLinesForInvoice(inv));
         } catch (Exception ex) {
             Logger.getLogger(MainView.class.getName()).log(Level.SEVERE, null, ex);
         }
-        fillTable(exportLinesList);
+        fillTable();
     }
 
     @Override
-    public void newDi() {
-         final Invoice i = (Invoice) getSelectedItem();
+    public void newDocument() {
+        final Invoice i = (Invoice) clientComboBox.getSelectedItem();
         AddInvoice aiv = new AddInvoice(i);
         aiv.addWindowListener(new WindowAdapter() {
 
             @Override
             public void windowDeactivated(WindowEvent evt) {
-                exportLinesList.clear();
+                documentLinesList.clear();
                 try {
-                    exportLinesList.addAll(null);
-                //    mainTable.updateUI();
+                    documentLinesList.addAll(null);
+                    //    mainTable.updateUI();
                 } catch (Exception ex) {
                     Logger.getLogger(MainView.class.getName()).log(Level.SEVERE, null, ex);
                 }
