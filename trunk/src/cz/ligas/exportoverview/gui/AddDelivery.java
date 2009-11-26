24cbae55e7a3723fcd38b10d697e76dc6510c301
miroslav.ligas <miroslav.ligas@9f4f964a-7ecc-11de-8c42-8d293cda74e2>
2009-11-26 16:11:36 +0000

    add documentline to document OK

--- trunk/src/cz/ligas/exportoverview/gui/AddDelivery.java
+++ trunk/src/cz/ligas/exportoverview/gui/AddDelivery.java
@@ -1,7 +1,12 @@
 package cz.ligas.exportoverview.gui;
 
+import cz.ligas.exportoverview.appli.DeliveryOps;
 import cz.ligas.exportoverview.db.Delivery;
+import cz.ligas.exportoverview.db.DeliveryLine;
 import cz.ligas.exportoverview.db.Document;
+import cz.ligas.exportoverview.db.Products;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
@@ -18,6 +23,20 @@ public class AddDelivery extends AddDocumentLineForm {
 
     @Override
     public void addDocumentLine() {
-                this.dispose();
+        DeliveryLine dl = new DeliveryLine();
+        int i =getnAmountIn();
+        float f = getmPriceIn();
+        dl.setAmount(i);
+        dl.setPrice(f);
+        dl.setDocument(delivery);
+        dl.setProd((Products)getProduct());
+        dl.setTotal(i*f);
+        //overenie vyplnenia
+        try {
+            DeliveryOps.addDeliveryLine(dl);
+        } catch (Exception ex) {
+            Logger.getLogger(ClientForm.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        this.dispose();
     }
 }
